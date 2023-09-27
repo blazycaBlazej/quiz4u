0b844f449dabb6729322813b24c5ec0a5adf0f9c
Błażej Błażyca <blazycablazej@gmail.com>
2023-09-27 12:58:25 +0200

    #feature/add frontend forgot password page

---
+++ app/email-wyslany/page.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import React from 'react'
+import { IconArrowNarrowLeft, IconLoader } from '@tabler/icons-react'
+
+import Link from 'next/link'
+import { useRouter, useSearchParams } from 'next/navigation'
+
+type FormValues = {
+	email: string
+	password: string
+}
+
+export default function page() {
+	const router = useRouter()
+
+	const clickHandler = () => {
+		//resend email
+	}
+	const searchParams = useSearchParams()
+
+	const email = searchParams.get('email')
+	return (
+		<main className='flex flex-col justify-center items-center gap-[20px] h-[calc(100vh-404px)] w-full'>
+			<h2 className='max-w-[410px] w-full text-3xl text-white '>Wiadomość została wysłana</h2>
+			<p className='max-w-[410px] w-full'>Link do zresetowania hasła otrzymasz pod adresem: {email}</p>
+			<p className='max-w-[410px] w-full'>Nie dostałeś wiadomości ? - kliknij poniższy przycisk</p>
+
+			<button
+				onClick={clickHandler}
+				className={`h-[50px] max-w-[410px] w-full bg-btn-violet-color  rounded-[20px] text-white cursor-pointer transition-colors hover:bg-btn-violet-color-hover`}>
+				Wyślij ponownie email
+			</button>
+
+			<Link href='/logowanie'>
+				<span className='text-white underline cursor-pointer  flex gap-2 text-right transition-colors hover:text-main-font-color'>
+					<IconArrowNarrowLeft /> Wróć do logowania
+				</span>
+			</Link>
+		</main>
+	)
+}
--- app/zapomnialem-hasla/page.tsx
+++ app/zapomnialem-hasla/page.tsx
@@ -1,10 +1,11 @@
 'use client'
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { IconEye, IconEyeOff, IconLoader } from '@tabler/icons-react'
+import { IconArrowNarrowLeft, IconLoader } from '@tabler/icons-react'
 import { useForm } from 'react-hook-form'
 import { DevTool } from '@hookform/devtools'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 type FormValues = {
 	email: string
@@ -12,23 +13,22 @@ type FormValues = {
 }
 
 export default function page() {
-	const [showPassword, setShowPassword] = useState(false)
+	const router = useRouter()
 	const [emailIsActive, setEmailIsActive] = useState(false)
-
-	const [passwordIsActive, setPasswordIsActive] = useState(false)
 
 	const form = useForm<FormValues>({
 		mode: 'onSubmit',
 	})
-	const { register, control, handleSubmit, formState, getValues } = form
+	const { register, handleSubmit, formState, getValues } = form
 	const { errors, isSubmitting } = formState
 
 	const onSubmit = (data: FormValues) => {
-		console.log('form sumbitted', data)
+		router.push(`/email-wyslany?email=${data.email}`)
 	}
 	return (
 		<main className='flex flex-col justify-center items-center gap-[20px] h-[calc(100vh-404px)] w-full'>
-			<span className='text-3xl text-white'>Logowanie</span>
+			<h2 className='max-w-[410px] w-full text-3xl text-white '>Zapomniałeś hasła ?</h2>
+			<p className='max-w-[410px] w-full'>Wprowadź swój adres e-mail, aby otrzymać instrukcje resetowania hasła.</p>
 			<form className='max-w-[410px] w-full' onSubmit={handleSubmit(onSubmit)} noValidate>
 				<div className='max-w-[410px] w-full relative mb-[20px]'>
 					<label
@@ -44,7 +44,7 @@ export default function page() {
 						id='email'
 						autoComplete='off'
 						{...register('email', {
-							required: 'email jest wymagany',
+							required: 'Email jest wymagany',
 							pattern: {
 								value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
 								message: 'Nieprawidłowy format maila',
@@ -55,40 +55,6 @@ export default function page() {
 					/>
 					<span className='text-sm text-error-color  block my-[4px]'>{errors.email?.message}</span>
 				</div>
-				<div className='relative'>
-					<label
-						htmlFor='password'
-						className={`absolute  pointer-events-none transition-top-left bg-main-backgorund px-[4px] ${
-							passwordIsActive || getValues('password') ? 'top-[-10px] left-[15px]' : 'top-[12px] left-[20px]'
-						}`}>
-						Hasło*
-					</label>
-					<input
-						className=' h-[50px] max-w-[410px] w-full pl-[20px] pr-[50px] bg-main-backgorund border-2  border-border-color rounded-[20px]'
-						type={showPassword ? 'text' : 'password'}
-						id='password'
-						{...register('password', {
-							required: 'Hasło jest wymagane',
-							minLength: {
-								value: 6,
-								message: 'Hasło jest za krótkie',
-							},
-						})}
-						onFocus={() => setPasswordIsActive(true)}
-						onBlur={() => setPasswordIsActive(false)}
-					/>
-					<span
-						className='block absolute top-[12px] right-[20px] cursor-pointer text-white transition-colors hover:text-main-font-color'
-						onClick={() => setShowPassword(!showPassword)}>
-						{showPassword ? <IconEyeOff /> : <IconEye />}
-					</span>
-					<span className='text-sm text-error-color  block my-[4px]'>{errors.password?.message}</span>
-				</div>
-				<Link href='/zapomnialem-hasla'>
-					<span className='text-white underline cursor-pointer block text-right my-[10px] transition-colors hover:text-main-font-color'>
-						Zapomniałeś hasła ?
-					</span>
-				</Link>
 
 				<button
 					disabled={isSubmitting || Object.keys(errors).length > 0}
@@ -100,11 +66,10 @@ export default function page() {
 					{isSubmitting ? <IconLoader /> : 'Zaloguj się'}
 				</button>
 			</form>
-			{/* <DevTool control={control} /> */}
 
-			<Link href='/rejestracja'>
-				<span className='text-white underline cursor-pointer block text-right transition-colors hover:text-main-font-color'>
-					Nie masz konta ? - Kliknji aby utworzyć konto
+			<Link href='/logowanie'>
+				<span className='text-white underline cursor-pointer  flex gap-2 text-right transition-colors hover:text-main-font-color'>
+					<IconArrowNarrowLeft /> Wróć do logowania
 				</span>
 			</Link>
 		</main>
